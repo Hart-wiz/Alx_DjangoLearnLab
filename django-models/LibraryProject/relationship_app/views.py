37dae518f95ed0ee5224b7a52e820bec0cc9bdd2
njimogu wisdom <njimoguwisdom@gmail.com>
2025-07-30 17:40:22 +0100

    template fix

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
 # Create your views here.
+# relationship_app/views.py
+
+from django.shortcuts import render, get_object_or_404
+from .models import Book, Library
+from django.views.generic import DetailView
+
+# ✅ Function-Based View: list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# ✅ Class-Based View: details of a specific library
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'  # So you can access it in the template
